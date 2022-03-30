555f44dc8a2dbc525fec148052cb2f78736f8dcf
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-03-30 19:38:51 -0300

    Desafio finalizado

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,12 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: calculate_lenght(list, 0)
+
+  defp calculate_lenght([], counter) do
+    counter
+  end
+
+  defp calculate_lenght([_head | tail], counter) do
+    counter = counter + 1
+    calculate_lenght(tail, counter)
   end
 end
